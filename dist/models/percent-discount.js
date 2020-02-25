db355cf3872d8cb7fe9405c86f20bce071bcc137
bharath-s95 <36877669+bharath-s95@users.noreply.github.com>
2020-02-25 18:31:41 +0530

    gitignore

--- dist/models/percent-discount.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var percentDiscountSchema = new mongoose_1.Schema({
-    discount_percentage: { type: mongoose_1.Schema.Types.Number, required: true },
-    maximum_amount: { type: mongoose_1.Schema.Types.Number, required: true }
-});
-exports.PercentDiscount = mongoose_1.model("PercentDiscount", percentDiscountSchema);
